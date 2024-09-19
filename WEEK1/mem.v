01addaa1e6d93fe8c5c2b182e1f7408c27b6ebce
Prajwal Kumar Shetty <109791458+Prajwalk1234@users.noreply.github.com>
2024-09-19 16:31:45 +0530

    Update mem.v

--- WEEK1/mem.v
+++ WEEK1/mem.v
@@ -20,28 +20,26 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module mem(
-    input [31:0]A,B,  //inputs
-    input [3:0]fun,     
-    output reg [31:0]Y,  //output 
-    output reg c_out
-);
-reg [31:0]temp;
-always @(*) begin
-    c_out = 1'b0; 
-    case(fun)
-        4'b0000: begin 
-            temp = A + B;           
-            Y = temp[31:0];    
-            c_out = temp[32];  
-        end
-        4'b0001: Y = A - B;       
-        4'b0010: Y = A & B;       
-        4'b0011: Y = A | B;        
-        4'b0100: Y = A ^ B;        
-        4'b0101: Y = A << B;        
-        4'b0110: Y = A >> B; 
-        default: Y = 32'b0;     
-    endcase
+`timescale 1ns / 1ps
+module mem(clk,rst,wr,rd,,address,Datain,Dataout);
+input clk,rst,wr,rd;
+input [7:0]Datain;
+input [3:0]address;
+reg [7:0] memory[0:10];
+output reg[7:0]Dataout;
+always@(posedge clk)
+begin
+if(rst)
+begin
+memory[address]=8'b0;
+end
+else
+begin
+case({wr,rd})
+2'b10:memory[address]=Datain;
+2'b01:Dataout=memory[address];
+default:Dataout=8'bz;
+endcase
+end
 end
 endmodule
