8e7e10289902b02adb21e7937f7f658dfd741f16
Prajwal Kumar Shetty <109791458+Prajwalk1234@users.noreply.github.com>
2024-09-19 16:32:58 +0530

    Update mem_tb.v

--- WEEK1/mem_tb.v
+++ WEEK1/mem_tb.v
@@ -21,65 +21,24 @@
 
 
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.09.2024 00:09:40
-// Design Name: 
-// Module Name: alu_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module mem_tb; 
-    reg [31:0]A,B;     
-    reg [3:0]fun;       
-    wire [31:0]Y; 
-    wire c_out;      
-        
-    mem U(.A(A),.B(B),.fun(fun),.Y(Y),.c_out(c_out));
-
-    integer i,j;
-    
-    initial 
-    begin
-        A = 32'b0; 
-        B = 32'b0;
-        fun = 4'b0; 
-        #10;
-        for (i = 0; i < 8; i = i + 1) 
-        begin
-            A = A + 1;              
-            for (j = 0; j < 8; j = j + 1)
-            begin
-               B = B + 1;  
-               fun = j; 
-               #10; 
-               $display("A = %h , B = %h , fun = %b , Y = %h , c_out = %b ", A , B , fun , Y , c_out);
-            end
-        end    
-        A = 32'bZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ;
-        B = 32'bZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ;
-        fun = 4'b0; //Z inputs
-        #10; 
-        $display("A = %h , B = %h , fun = %b , Y = %h , c_out = %b ", A , B , fun , Y , c_out);      
-        A = 32'bXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
-        B = 32'bXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
-        fun = 4'b0; //X inputs
-        #10;
-        $display("A = %h , B = %h , fun = %b , Y = %h , c_out = %b ", A , B , fun , Y , c_out);
-        $finish;
-    end
+module mem_tb();
+reg clk,rst,wr,rd;
+reg [7:0]Datain;
+reg [3:0]address;
+//reg [7:0]memory[0:10];
+wire [7:0]Dataout;
+mem U0(.clk(clk),.rst(rst),.wr(wr),.rd(rd),.address(address),.Datain(Datain),.Dataout(Dataout));
+initial
+begin
+#10 clk=0;
+#10 rst=0;address=4'b0011;wr=1;rd=0;Datain=8'b00001111;
+#10 rst=0;address=4'b0011;wr=0;rd=1;
+#10 rst=0;address=4'b0011;wr=1;rd=1;
+#10 rst=0;address=4'b0011;wr=0;rd=0;
+#10 rst=0;address=4'b0111;wr=0;rd=1;
+#10 rst=0;address=4'b1111;wr=1;rd=0;Datain=8'b00011111;
+#10 rst=1;
+#20 $finish;
+end
+always #5 clk=~clk;
 endmodule
-
